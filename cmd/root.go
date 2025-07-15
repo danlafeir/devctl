5f3e4e2cdbff39e2982cb0ce441d7c725e452a70
Dan Lafeir <danlafeir@users.noreply.github.com>
2025-07-14 21:52:57 -0500

    Hide the help cmd

--- cmd/root.go
+++ cmd/root.go
@@ -34,4 +34,7 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Disable the help command
+	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
 }
